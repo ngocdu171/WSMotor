ab554278682ce20e623da523c88b2895fbde351c
ngocdu <ngocdu171@gmail.com>
2021-03-12 13:09:10 +0200

    complete login function

--- routes/user.js
+++ routes/user.js
@@ -38,5 +38,31 @@ router.post('/register', async (req, res) =>{
     })
 });
 
+router.get('/login', async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if(username && password) {
+        const user = await User.find({username: username});
+        if(user.length > 0) {
+            await bcrypt.compare(password, user[0].password)
+            .then(results => {
+                if(results === true) {
+                    res.json({message: "login successfully!"});
+                    console.log(user);
+                }
+                else {
+                    res.json({message: "password does not match!!!!"});
+                }
+            })
+        }
+        else {
+            res.json({message: "username doesn't exist!"})
+        }
+    }
+    else {
+        res.send("fill the fields!");
+    }
+})
+
 
 module.exports = router;
